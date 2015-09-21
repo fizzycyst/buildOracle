a44aaf96947afe339b4e2dfeab43049260e83544
Marko Wallin <marko.wallin@iki.fi>
2015-09-21 15:25:21 +0300

    Remove puppet modules Install puppet modules with script

--- vagrant-wls12c-centos6/puppet/modules/wls/lib/puppet/parser/functions/slash_replace.rb
+++
@@ -1,9 +0,0 @@
-# restart the puppetmaster when changed
-module Puppet::Parser::Functions
-  newfunction(:slash_replace, :type => :rvalue) do |args|
-    
-    newValue    = args[0].strip.gsub("/","\\")
-    return newValue
-
-  end
-end
