a29cec512816dee4cce14e4b6cc3a36d09b4a858
Marko Wallin <marko.wallin@iki.fi>
2015-09-23 16:15:30 +0300

    Update scripts for installing Eclipse plugins Divide dev tools installation scripts

--- vagrant-ubuntu/install-dev-environment.sh
+++ vagrant-ubuntu/install-dev-environment.sh
@@ -8,11 +8,9 @@ sudo echo "LC_TIME=fi_FI.UTF-8" >> /etc/environment
 
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
-sudo apt-get -y upgrade
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections 
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo apt-get -y install oracle-java6-installer
 sudo apt-get -y install oracle-java8-installer
 
 sudo echo "allowed_users=anybody" > /etc/X11/Xwrapper.config
-
-sudo loadkeys fi
